23916259cb37ccff5d373612c46f3ffa835f9ff4
10994015 <a0938599191@gmail.com>
2023-03-13 00:51:06 +0800

    create vue vuex vue-router

---
+++ backend/src/router/index.js
@@ -0,0 +1,23 @@
+
+import Dashboard from "../views/dashboard.vue"
+import Login from "../views/login.vue"
+import {createRouter, createWebHistory} from "vue-router"
+const routes = [
+    {
+        path:'/',
+        name:'dashboard',
+        component:Dashboard,
+    },
+    {
+        path:'/login',
+        name:'login',
+        component:Login,
+    },
+   
+];
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
