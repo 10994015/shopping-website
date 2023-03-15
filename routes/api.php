d2392a214380e32fd33a60251ce1993b553d9ecd
10994015 <a0938599191@gmail.com>
2023-03-15 15:42:16 +0800

    0315

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum', 'admin'])->group(function(){
     Route::get('/user', [AuthController::class, 'getUser']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('/product', ProductController::class);
+
 });
 
 Route::post('/login', [AuthController::class, 'login']);
