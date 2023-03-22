ca8fb5bf6c54f3833907a1959e88223154a0829b
10994015 <a0938599191@gmail.com>
2023-03-23 07:50:57 +0800

    前台完成

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,37 @@
-<x-guest-layout>
+<x-app-layout>
+    <div class="login">
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <h2>登入</h2>
+            <label for="">
+                <p>電子郵件 <span>*</span></p>
+                <input type="email" name="email" :value="old('email')" required autofocus />
+            </label>
+            <label for="">
+                <p>密碼 <span>*</span></p>
+                <input type="password" name="password" required />
+            </label>
+            <label for="remember_me" class="flex items-center">
+                <input type="checkbox" id="remember_me" name="remember" class="mr-1" />記住我
+            </label>
+            <label for="">
+                <button type="submit">登入</button>
+            </label>
+            <div>
+                <a href="/register">還沒有帳號嗎？點擊註冊</a>
+                <a href="/">忘記密碼？</a>
+            </div>
+        </form>
+    </div>
+
+    @push('scripts')
+    <script>
+        document.querySelector('header').classList.add('isActive');
+    </script>
+    @endpush
+</x-app-layout>
+
+{{-- <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -6,23 +39,19 @@
             </a>
         </x-slot>
 
-        <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
-            <!-- Email Address -->
             <div>
                 <x-label for="email" :value="__('Email')" />
 
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
-            <!-- Password -->
             <div class="mt-4">
                 <x-label for="password" :value="__('Password')" />
 
@@ -32,7 +61,6 @@
                                 required autocomplete="current-password" />
             </div>
 
-            <!-- Remember Me -->
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
@@ -53,4 +81,4 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+</x-guest-layout> --}}
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -8,7 +8,6 @@
         <a href="/store">線上商店</a>
         <a href="">關於我們</a>
         <a href="">聯絡我們</a>
-        <a href="">我的帳號</a>
         <a href="javascript:;" id="cart-btn">
             <p>$ 0</p>
             <div class="icon">
@@ -16,6 +15,18 @@
                 <i class="fa-solid fa-bag-shopping"></i>
             </div>
         </a>
+        @if(Auth::check())
+        <a href="javascript:;" class="myaccount">
+            <i class="fa-solid fa-user"></i>
+            <ol>
+                <p><i class="fa-solid fa-user"></i>我的帳號</p>
+                <p><i class="fa-solid fa-file"></i>訂單查詢</p>
+                <form method="post" action="{{route('logout')}}" id="logoutForm" onclick="logoutForm.submit()"><p>@csrf<i class="fa-solid fa-right-from-bracket"></i>登出</p></form>
+            </ol>
+        </a>
+        @else
+        <a href="/login" ><i class="fa-solid fa-user"></i></a>
+        @endif
     </nav>
 </header>
 
