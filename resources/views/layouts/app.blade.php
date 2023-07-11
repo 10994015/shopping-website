fa7b144f776e01906fd5444e2291610f3c9fec84
10994015 <a0938599191@gmail.com>
2023-07-12 04:24:38 +0800

    update title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         
-        <title>{{ config('app.name', '房子ROW') }}</title>
+        <title>房子ROW</title>
         <!-- Scripts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
