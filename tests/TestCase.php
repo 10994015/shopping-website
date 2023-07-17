dbce8101aada87b66b6ffe4178eb60d793378f4a
李承諺 <75940272+10994015@users.noreply.github.com>
2023-07-18 00:48:32 +0800

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,8 +8,4 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-    protected function setUp():void{
-        parent::setUp();
-        $this->withoutVite();
-    }
 }
