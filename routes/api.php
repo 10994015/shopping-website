31fa0b700899255d850dbc337a664194194b1c95
10994015 <a0938599191@gmail.com>
2023-07-19 16:31:20 +0800

    0719

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', 'admin'])->group(function(){
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'getUser']);
 
     Route::apiResource('products', ProductController::class);
     Route::post('/isExistProduct', [ProductController::class, 'isExistProduct']);
@@ -28,6 +29,5 @@ Route::middleware(['auth:sanctum', 'admin'])->group(function(){
     Route::delete('/category/{id}', [CategoryController::class, 'destroy']);
 
 });
-Route::get('/user', [AuthController::class, 'getUser']);
 
 Route::post('/login', [AuthController::class, 'login']);
